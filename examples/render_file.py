e6343218e4b0321ae30c381a58e480ede0f13ea1
zhaov <kaihaosw@gmail.com>
2014-03-22 18:17:59 +0800

    remove examples

--- examples/render_file.py
+++
@@ -1,7 +0,0 @@
-from cyanbird import render, GET, run
-
-@GET("/")
-def index(request):
-    return render("templates/index.html", dict(title="built-in template", page="main"))
-
-run()
--- examples/serve_static_file.py
+++
@@ -1,7 +0,0 @@
-from cyanbird import GET, run, serve_file
-
-@GET("/license")
-def index(request):
-    return serve_file("LICENSE", dir="static")
-
-run()
