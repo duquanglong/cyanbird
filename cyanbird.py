ee8ef12fa4cffb710619d01f89a770a19a6b575f
zhaov <kaihaosw@gmail.com>
2014-03-26 06:59:58 +0800

    update license

--- cyanbird.py
+++ cyanbird.py
@@ -1,5 +1,27 @@
 # -*- coding: utf-8 -*-
-__license__ = "MIT"
+__license__ = """
+The MIT License (MIT)
+
+Copyright (c) 2014 Zhao Wei
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+"""
 __version__ = "0.20"
 __author__ = "Zhao Wei <kaihaosw@gmail.com>"
 import re
@@ -31,6 +53,7 @@ class HTTPError(CyanBirdException):
 
 
 class TemplateException(Exception):
+    """ Basic Exception for Template. """
     def __init__(self, msg):
         self.msg = msg
 
@@ -486,7 +509,12 @@ class ServeFile(object):
 #  #for @foo in @foos:
 #  #include
 class Template(object):
-    pass
+    """ Template System for Cyanbird. """
+    def __init__(self, text):
+        pass
+
+    def render(self):
+        pass
 
 
 ##,---------------------------
